c6a5cf9c88105b90c6eeae250d10bc21c4f1fc55
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-07-21 01:48:16 +0530

    This conatins the modified code where the outpust of the Hero page resides with the zerotha page.

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero() {
     return (  
         <div className="container p-5 mb-5">
-            <div className="row text-center">
+            <div className="row text-center" style={{ marginLeft: "100px", marginRight: "100px" }}>
                 <img src= 'media/images/homeHero.png' alt='Hero Image'className='mb-5'/>
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, bonds, ETFs, and more.</p>
