d6cf223afe3f050bd6e075d9368686f51733976e
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-07-23 17:36:09 +0530

    Add Navbar component for site-wide navigation and improved user experience

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return ( 
             <nav className="navbar navbar-expand-lg border-bottom" style={{backgroundColor: "#FFF"}}>
                 <div className="container p-3" style={{height: "20%"}}>
-                    <a className="navbar-brand" href="#"><img src='media/images/logo.svg' alt='logo' style={{width: "25%", display: "flex", justifyContent: "center"}}></img></a>
+
+                    <Link className="navbar-brand" to="/"><img src='media/images/logo.svg' alt='logo' style={{width: "25%", display: "flex", justifyContent: "center"}}></img></Link>
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarScroll">
                         <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{"--bs-scroll-height": "100px"}}>
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="#">Signup</a>
+                                <Link className="nav-link" aria-current="page" to="/signup">Signup</Link>
                             </li>
                             <li className="nav-item ">
-                                <a className="nav-link" href="#">About</a>
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Products</a>
+                                <Link className="nav-link" to="/product">Products</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <Link className="nav-link" to="pricing">Pricing</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Support</a>
+                                <Link className="nav-link" to="support">Support</Link>
                             </li>
                             <li className="nav-item mt-1" >
-                                <a className="nav-link" href="#"><i class="fa fa-bars" aria-hidden="true"></i></a>
+                                <Link className="nav-link" to="#"><i class="fa fa-bars" aria-hidden="true"></i></Link>
                             </li>
                             
                         </ul>
