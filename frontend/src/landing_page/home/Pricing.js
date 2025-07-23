c6fc3a738332cc369f10c1e5d5694840f5d3d24e
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-07-23 17:31:37 +0530

    This is the Pricng page

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Pricing() {
     return ( 
         <div className='container mb-5'>
-            <div className='row'>
+            <div className='row' style={{ marginLeft: "100px", marginRight: "100px" }}>
                 <div className='col-4'>
                     <h1 className='mb-3 fs-2'>Unbeatable Pricing</h1>
                     <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges </p>
