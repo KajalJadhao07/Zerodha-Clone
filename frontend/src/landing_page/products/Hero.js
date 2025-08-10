7ba01c0ff39910e737dfc999513fd6a6e7029853
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-08-10 13:45:38 +0530

    Add Hero.js component for product section hero banner and introduction

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero Section</h1>
+        <div className='container border-bottom' style={{textAlign: "center", padding: "75px 0 100px"}}>
+            <h3>Zerodha Products</h3>
+            <p className='fs-5'>Sleek, modern, and intuitive trading platforms</p>
+            <p>Check out our <a href='' style={{textDecoration: "none"}}>investment offerings →</a></p>
+        </div>
      );
 }
 
