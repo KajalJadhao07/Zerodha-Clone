a3b67ec156998843327fea9f8ddc7a6b1b4ff13d
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-08-10 13:46:43 +0530

    Add LeftSection.js component for product page sidebar and navigation

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-function LeftSection() {
-    return ( 
-        <h1>Left Section</h1>
-     );
+function LeftSection({ imageURL, productName, description, tryDemoLink, learnMoreLink, googlePlayLink, appStoreLink }) {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 p-5 ' style={{paddingLeft: "250px"}}>
+                    <img src={imageURL} alt={productName} />
+                </div>
+                <div className='col-6 p-5 mt-5 mb-5'>
+                    <h4 >{productName}</h4>
+                    <p className='mt-4' style={{marginRight: "100px", paddingRight: "100px", lineHeight: "1.8", fontSize: "1.1em" }}>{description}</p>
+
+                    <div>
+                        <a href={tryDemoLink} style={{ textDecoration: "none"}}>Try Demo →</a>
+                        <a href={learnMoreLink} style={{ textDecoration: "none", marginLeft:"50px" }}>Learn More →</a>
+                    </div>
+
+                    <div className='mt-3'>
+                        <a href={googlePlayLink}><img src='\media\images\googlePlayBadge.svg' alt='Google Play Store' /></a>
+                        <a href={appStoreLink} style={{ marginLeft: "50px" }}><img src='\media\images\appstoreBadge.svg' alt='Apple App Store' /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LeftSection;
